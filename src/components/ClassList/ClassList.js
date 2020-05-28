762d4adfd8602ecbb08c52932e9b0e7916e88491
bbdoggett3 <bbdoggett3@gmail.com>
2020-05-28 14:41:16 -0600

    create sudent information

--- src/components/ClassList/ClassList.js
+++ src/components/ClassList/ClassList.js
@@ -1,18 +1,40 @@
 import React, { Component } from 'react';
+import axios from 'axios';
+import {Link} from 'react-router-dom';
 
 export default class ClassList extends Component {
   constructor() {
     super()
+
+    this.state = {
+      students: []
+    }
     
   }
 
+  componentDidMount() {
+    axios.get(`http://localhost:3005/students?class=${this.props.match.params.class}`).then(res => {
+      this.setState({
+        students: res.data
+      })
+    }).catch(error => console.log(error, 'this is not working'))
+  }
+
   render() {
+    const mappedClass = this.state.students.map((student, id) => (
+      <Link key={id} to= {`/student/${student.id}`} >
+        <h3> {student.first_name} {student.last_name}</h3>
+      </Link>
+    ));
+
+    console.log(this.props)
     return (
       <div className="box">
-        <h1></h1>
+        <h1>{this.props.match.params.class}</h1>
         <h2>ClassList:</h2>
-
+        {mappedClass}
       </div>
     )
   }
 }
+
